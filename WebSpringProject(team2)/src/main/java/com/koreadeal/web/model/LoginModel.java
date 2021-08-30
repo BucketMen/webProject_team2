753a574ad639ec4277d44bbfdf1f40c785002c19
Youjeongmu <bkul286040@gmail.com>
2021-08-30 18:00:42 +0900

    2021-08-30 commit

--- WebSpringProject(team2)/src/main/java/com/koreadeal/web/model/LoginModel.java
+++ WebSpringProject(team2)/src/main/java/com/koreadeal/web/model/LoginModel.java
@@ -1,5 +1,42 @@
 package com.koreadeal.web.model;
 
 public class LoginModel {
-
+	private String login_id;
+	private String login_pwd;
+	private String login_flag;
+	private String submit_flag;
+	private String blockdata;
+	
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	public String getLogin_pwd() {
+		return login_pwd;
+	}
+	public void setLogin_pwd(String login_pwd) {
+		this.login_pwd = login_pwd;
+	}
+	public String getLogin_flag() {
+		return login_flag;
+	}
+	public void setLogin_flag(String login_flag) {
+		this.login_flag = login_flag;
+	}
+	public String getSubmit_flag() {
+		return submit_flag;
+	}
+	public void setSubmit_flag(String submit_flag) {
+		this.submit_flag = submit_flag;
+	}
+	public String getBlockdata() {
+		return blockdata;
+	}
+	public void setBlockdata(String blockdata) {
+		this.blockdata = blockdata;
+	}
+	
+	
 }
